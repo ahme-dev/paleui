6401edf78c97ff353b0733507e88e33f58ba0199
Ahmed <mail@ahme.dev>
2025-07-28 19:11:15 +0300

    feat(site): make code highlighting optional and add to all pages

--- packages/site/assets/code.js
+++ packages/site/assets/code.js
@@ -1,30 +1,90 @@
-import { codeToHtml } from "https://esm.sh/shiki@3.0.0";
+//
+// Highlighter
+//
+
+let shikiEnabled = false;
+function getShiki() {
+	try {
+		shikiEnabled = localStorage.getItem("shiki") === "true";
+	} catch (_) {}
+}
+getShiki();
+function toggleShiki() {
+	localStorage.setItem("shiki", !shikiEnabled);
+	shikiEnabled = !shikiEnabled;
+}
+
+async function highlightHtml(str) {
+	let html = "";
+
+	if (shikiEnabled) {
+		const mod = await import("https://esm.sh/shiki@3.0.0");
+		html = await mod.codeToHtml(str, {
+			lang: "html",
+			theme: "min-light",
+		});
+	} else {
+		const escapedStr = str
+			.replace(/&/g, "&amp;")
+			.replace(/</g, "&lt;")
+			.replace(/>/g, "&gt;")
+			.replace(/"/g, "&quot;")
+			.replace(/'/g, "&#39;");
+		html = `<pre><code>${escapedStr}</code></pre>`;
+	}
+
+	return html;
+}
+
+function addHighlightButton(to) {
+	if (to.querySelector(".highlight-btn")) return;
+
+	const highlightButton = document.createElement("button");
+	highlightButton.textContent = shikiEnabled ? "✪" : "☲";
+	highlightButton.className = "icon outline highlight-btn";
+	const text = shikiEnabled ? "Turn off highlighting" : "Turn on highlighting";
+	highlightButton.setAttribute("aria-label", text);
+	highlightButton.title = text;
+	highlightButton.style =
+		"position: absolute; top: 10px; right: 45px; z-index: 1000; cursor: pointer;";
+
+	highlightButton.addEventListener("click", () => {
+		toggleShiki();
+		const confirmText = shikiEnabled
+			? "Reload the page to turn on highlighting? This will load shikijs and increase bundle size on the site."
+			: "Reload the page to turn off highlighting?";
+		if (confirm(confirmText)) location.reload();
+	});
+	to.appendChild(highlightButton);
+}
+
+//
+// Replacer
+//
 
 async function replaceWithCode(from, to) {
 	let codeContent = from.innerHTML;
 
 	const firstLineTabCount =
-		codeContent.split("\n")[1].match(/^\t+/)?.[0].length || 0;
+		codeContent.split("\n")[1]?.match(/^\t+/)?.[0]?.length || 0;
 
 	codeContent = from.innerHTML
 		.split("\n")
-		.map((line) => line.replace(new RegExp(`^\\t{0,${firstLineTabCount}}`), ""))
+		.map((line) => line.replace(new RegExp(`^\t{0,${firstLineTabCount}}`), ""))
 		.slice(1, -1)
 		.join("\n");
 
-	to.innerHTML = await codeToHtml(codeContent, {
-		lang: "html",
-		theme: "min-light",
-	});
+	to.innerHTML = await highlightHtml(codeContent);
 
-	// const closestObject = to.closest("object");
-	// closestObject.style.height = "auto";
-	// closestObject.style.maxHeight = "calc((100vw * 5) / 9)";
-	// closestObject.style.aspectRatio = "auto";
+	addCopyButton(to, codeContent);
+}
+
+function addCopyButton(to, codeContent) {
+	if (to.querySelector(".copy-btn")) return;
 
 	const copyButton = document.createElement("button");
-	copyButton.textContent = "⏍";
-	copyButton.className = "icon secondary";
+	copyButton.textContent = "⎘";
+	copyButton.className = "icon outline copy-btn";
 	copyButton.setAttribute("aria-label", "Copy to clipboard");
 	copyButton.title = "Copy to clipboard";
 	copyButton.style =
@@ -36,28 +96,35 @@ async function replaceWithCode(from, to) {
 			.then(() => {
 				copyButton.textContent = "✔";
 				setTimeout(() => {
-					copyButton.textContent = "⏍";
+					copyButton.textContent = "⎘";
 				}, 2000);
 			})
 			.catch((_) => {
 				copyButton.textContent = "✘";
 				setTimeout(() => {
-					copyButton.textContent = "⏍";
+					copyButton.textContent = "⎘";
 				}, 2000);
 			});
 	});
 	to.appendChild(copyButton);
 }
 
+//
+// Watch and replace all fields
+//
+
+const ATTRIBUTE = "data-to-code";
+const ATTRIBUTE_WITH_BRACKETS = `[${ATTRIBUTE}]`;
+
 function updateTabs() {
-	const codeBlocks = document.querySelectorAll("[data-to-code]");
-	codeBlocks.forEach(async (block) => {
-		const attr = block.getAttribute("data-to-code");
+	document.querySelectorAll(ATTRIBUTE_WITH_BRACKETS).forEach(async (block) => {
+		const attr = block.getAttribute(ATTRIBUTE);
 		const elementToReplace =
 			attr.trim() === "" ? block : document.querySelector(attr) || block;
 
-		block.removeAttribute("data-to-code");
+		block.removeAttribute(ATTRIBUTE);
 		await replaceWithCode(block, elementToReplace);
+		addHighlightButton(elementToReplace);
 	});
 }
 
@@ -70,5 +137,4 @@ const observer = new MutationObserver(() => {
 		updateTabs();
 	}, 100);
 });
-
 observer.observe(document.body, { childList: true, subtree: true });
